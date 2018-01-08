8fafe6c4e215a6ff29e8dbfe36d1fa0aee73eb22
=DevopsProfessionalUK <=DevOpsProfessionalUK@gmail.com>
2018-01-08 20:46:09 +0000

    added http and ssh port in ingress and all traffic

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow-ssh" {
   vpc_id = "${aws_vpc.main.id}"
   name = "allow-ssh"
-  description = "security group that allows ssh and all egress traffic"
+  description = "security group that allows ssh/http and all egress traffic"
   egress {
       from_port = 0
       to_port = 0
@@ -19,3 +19,21 @@ tags {
     Name = "allow-ssh"
   }
 }
+egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+tags {
+    Name = "allow-http"
+  }
+}
+
