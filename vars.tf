012a43a68d87f9ea51d366de5aabbc59ead0d472
=DevopsProfessionalUK <=DevOpsProfessionalUK@gmail.com>
2018-01-08 20:42:45 +0000

    updated the ubuntu image AMI

--- vars.tf
+++ vars.tf
@@ -2,17 +2,15 @@ variable "AWS_REGION" {
   default = "eu-west-1"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "mykey2"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "mykey2.pub"
 }
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    eu-west-1 = "ami-8fd760f6"
   }
 }
 variable "INSTANCE_DEVICE_NAME" {
