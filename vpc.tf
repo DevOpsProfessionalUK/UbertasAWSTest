6c19e39b9b14ddb28420426761b0b52b18388f2b
=DevopsProfessionalUK <=DevOpsProfessionalUK@gmail.com>
2018-01-08 20:44:39 +0000

    two public subnet & two Private subnet & 1 Internet gateway

--- vpc.tf
+++ vpc.tf
@@ -32,16 +32,6 @@ resource "aws_subnet" "main-public-2" {
         Name = "main-public-2"
     }
 }
-resource "aws_subnet" "main-public-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.3.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1c"
-
-    tags {
-        Name = "main-public-3"
-    }
-}
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
@@ -60,16 +50,6 @@ resource "aws_subnet" "main-private-2" {
 
     tags {
         Name = "main-private-2"
-    }
-}
-resource "aws_subnet" "main-private-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.6.0/24"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1c"
-
-    tags {
-        Name = "main-private-3"
     }
 }
 
@@ -102,9 +82,5 @@ resource "aws_route_table_association" "main-public-1-a" {
 }
 resource "aws_route_table_association" "main-public-2-a" {
     subnet_id = "${aws_subnet.main-public-2.id}"
-    route_table_id = "${aws_route_table.main-public.id}"
-}
-resource "aws_route_table_association" "main-public-3-a" {
-    subnet_id = "${aws_subnet.main-public-3.id}"
     route_table_id = "${aws_route_table.main-public.id}"
 }
